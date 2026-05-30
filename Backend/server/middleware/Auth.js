7bf84c047ae49a17e914c08561b45db6ebf83b5b
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-05-30 17:17:11 +0530

    Update Auth.js

--- Backend/server/middleware/Auth.js
+++ Backend/server/middleware/Auth.js
@@ -9,29 +9,16 @@ const auth = (req, res, next) => {
             return res.status(401).json({ message: "No token provided" })
         }
 
-<<<<<<< HEAD
         const actualToken = token.split(" ")[1]   // ✅ remove "Bearer"
 
         const decoded = jwt.verify(actualToken, SECRET_KEY)
 
         req.userid = decoded.id   // ✅ VERY IMPORTANT
-=======
-        const actualToken = token.split(" ")[1]   
 
-        const decoded = jwt.verify(actualToken, SECRET_KEY)
-
-        req.userid = decoded.id   
->>>>>>> 82215cb8c94d441cfeccaf739c52fd84b83763c3
-
-        next()
     } catch (error) {
         console.log(error)
         res.status(401).json({ message: "Invalid token" })
     }
 }
 
-<<<<<<< HEAD
 module.exports = auth
-=======
-module.exports = auth
->>>>>>> 82215cb8c94d441cfeccaf739c52fd84b83763c3
