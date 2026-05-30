680b7a4216703aa7089222a429c00dc39cc5dad0
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-05-30 17:03:56 +0530

    Update productRoutes.js

--- Backend/server/routes/productRoutes.js
+++ Backend/server/routes/productRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const { addProduct, getProducts, getproductById, UpdateProduct, deleteProduct } = require('../controller/productController');
 const upload = require('../middleware/imageUpload')
@@ -10,17 +9,4 @@ route.get("/getproductById/:id", getproductById)
 route.put('/UpdateProduct/:id', upload.single('productimage'), UpdateProduct)  
 route.delete('/deleteProduct/:id', deleteProduct)
 
-=======
-const express = require('express');
-const { addProduct, getProducts, getproductById, UpdateProduct, deleteProduct } = require('../controller/productController');
-const upload = require('../middleware/imageUpload')
-const route = express.Router()
-
-route.post('/addProduct', upload.single('productimage'), addProduct)
-route.get('/getProducts', getProducts)
-route.get("/getproductById/:id", getproductById)  
-route.put('/UpdateProduct/:id', upload.single('productimage'), UpdateProduct)  
-route.delete('/deleteProduct/:id', deleteProduct)
-
->>>>>>> 82215cb8c94d441cfeccaf739c52fd84b83763c3
 module.exports = route
