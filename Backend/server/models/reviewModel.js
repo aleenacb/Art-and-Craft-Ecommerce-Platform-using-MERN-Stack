7955204a5b03adec41166732c20978d71818fbb1
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-05-30 16:49:07 +0530

    Remove duplicate review schema definition

--- Backend/server/models/reviewModel.js
+++ Backend/server/models/reviewModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
@@ -11,18 +10,4 @@ const reviewSchema = new mongoose.Schema({
     tags: { type: [String], default: [] },  
 }, { timestamps: true });
 
-=======
-const mongoose = require('mongoose');
-
-const reviewSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    userName: { type: String, default: 'Anonymous' },
-    productId: { type: String },
-    rating: { type: Number, required: true, min: 1, max: 5},
-    title: { type: String, default: '' },
-    review: { type: String, required: true },
-    tags: { type: [String], default: [] },  
-}, { timestamps: true });
-
->>>>>>> 82215cb8c94d441cfeccaf739c52fd84b83763c3
 module.exports = mongoose.model('Review', reviewSchema);
