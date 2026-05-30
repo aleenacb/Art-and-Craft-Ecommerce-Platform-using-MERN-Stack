e34aadad1c5c5100c368712a9f55204b6b1ac14b
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-05-30 16:47:53 +0530

    Fix ProductModel.js by resolving merge conflicts
    Removed merge conflict markers and cleaned up code.

--- Backend/server/models/ProductModel.js
+++ Backend/server/models/ProductModel.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
@@ -16,23 +16,4 @@ const productSchema = new mongoose.Schema({
     timestamps: true  
 });
 
-=======
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    price: { type: Number },
-    quantity: { type: Number },
-    categoryId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'category'
-    },
-    productimage:{type:String}
-    
-}, {
-    timestamps: true  
-});
-
->>>>>>> 82215cb8c94d441cfeccaf739c52fd84b83763c3
 module.exports = mongoose.model('product', productSchema);
